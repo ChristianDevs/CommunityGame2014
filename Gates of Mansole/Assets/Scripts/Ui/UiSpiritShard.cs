a30a33915563a3b78c98c3a4fb719893f1363360
codergreen <jnskeer@gmail.com>
2014-11-10 20:00:04 -0500

    spirit shards added
    spirit shard animation added and count added

---
+++ Gates of Mansole/Assets/Scripts/Ui/UiSpiritShard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UiSpiritShard : MonoBehaviour {
+
+	public GameObject textTexture;
+
+
+	// Use this for initialization
+	void Start () {
+		textTexture.GetComponent<TextMesh>().text="x"+Player.spiritShards;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textTexture.GetComponent<TextMesh>().text="x"+Player.spiritShards;
+	}
+}
