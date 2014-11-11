a181a8f4274f876cee45b6a0763180c8363014d6
codergreen <jnskeer@gmail.com>
2014-11-11 10:55:58 -0500

    added unit details
    details of unit stats added

---
+++ Gates of Mansole/Assets/Scripts/Ui/UiUnitSelect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiUnitSelect : MonoBehaviour {
+	
+	public GameObject factionText;
+	public GameObject expText;
+	public GameObject attackText;
+	public GameObject defenseText;
+	public GameObject spiritText;
+	public GameObject speedText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SelectUnit(GameObject go){
+		GomUnit gom = go.GetComponent<GomUnit>();
+		factionText.GetComponent<TextMesh>().text = "Faction: "+gom.faction.ToString();
+		expText.GetComponent<TextMesh>().text = "Level: "+gom.exp.level;
+		attackText.GetComponent<TextMesh>().text = "x"+gom.getStats().attack;
+		defenseText.GetComponent<TextMesh>().text = "x"+gom.getStats().defense;
+		spiritText.GetComponent<TextMesh>().text = "x"+gom.getStats().spirit;
+		speedText.GetComponent<TextMesh>().text = "x"+gom.getStats().speed;
+
+	}
+}
