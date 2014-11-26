2ab29a1970740b7ff2d2b83077c2b229f7666d52
murrayk012000 <murrayk012000@yahoo.com>
2014-11-25 16:39:17 -0800

    Showing Spirit Orbs and fixing Spirit Shard animation

---
+++ Gates of Mansole/Assets/Scripts/Ui/UiSpiritOrbs.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiSpiritOrbs : MonoBehaviour {
+	
+	public GameObject textTexture;
+	
+	
+	// Use this for initialization
+	void Start () {
+		textTexture.GetComponent<TextMesh>().text="x"+Player.spiritOrbs;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textTexture.GetComponent<TextMesh>().text="x"+Player.spiritOrbs;
+	}
+}
