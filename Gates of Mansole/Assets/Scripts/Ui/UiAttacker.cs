b853db6bb3da8a6edb5d03db661e3f089f7e7e0b
Dylen Brandon Rivera <dylenbrivera@gmail.com>
2015-01-22 12:06:37 -0500

    Add space between colon and attacker value.
    UiAttacker code cleanup.

--- Gates of Mansole/Assets/Scripts/Ui/UiAttacker.cs
+++ Gates of Mansole/Assets/Scripts/Ui/UiAttacker.cs
@@ -5,13 +5,9 @@ public class UiAttacker : MonoBehaviour {
 
 	public GameObject world;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		TextMesh txt = this.GetComponent<TextMesh> ();
-		txt.text = "Attackers:" + world.GetComponent<WorldController>().letThroughAttackers;
+		txt.text = "Attackers: " + world.GetComponent<WorldController>().letThroughAttackers;
 	}
 }
