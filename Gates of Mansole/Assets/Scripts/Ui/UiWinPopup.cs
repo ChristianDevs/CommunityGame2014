6797e6ddb4cb37f74c7ecc65a688e4def52a58d5
codergreen <jnskeer@gmail.com>
2015-02-03 14:34:56 -0500

    Win and lose popups
    Added popups for with and lose

---
+++ Gates of Mansole/Assets/Scripts/Ui/UiWinPopup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiWinPopup : MonoBehaviour {
+	void Start () {
+		
+	}
+	
+	void buttonPush(string buttonName) {
+		switch (buttonName) {
+		case "Retry":
+			Application.LoadLevel(Application.loadedLevelName);
+			break;
+		case "Continue":
+			Application.LoadLevel("LevelSelect");
+			break;
+		default:
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
